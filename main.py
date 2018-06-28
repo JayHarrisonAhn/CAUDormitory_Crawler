f611e55d837db6070e6434becab4cbf72b02f528
lbr0452000 <lbr0452000@naver.com>
2018-06-29 03:59:57 +0900

    push notification added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from extract import Notice
 import extract
 import firebase_admin
+from firebase_admin import messaging
 from firebase_admin import credentials
 from firebase_admin import db
 
@@ -14,6 +15,36 @@ firebase_admin.initialize_app(cred, {
 })
 root = db.reference()
 notice = Notice()
+
+pushNotices=[]
+pushNoticeTitles=[]
+snapshot = root.child('Seoul_Bluemir/Notices').get()
+for i in range(0,len(notice.IDNumbers)):
+    isExisted = 0
+    for key in snapshot:
+        if key==notice.IDNumbers[i] :
+            isExisted = 1
+    if isExisted==0:
+        pushNotices.append(notice.IDNumbers[i])
+        pushNoticeTitles.append(notice.Titles[i])
+        print(notice.IDNumbers[i])
+
+snapshot = root.child('Users').get()
+
+for key in snapshot:
+    for i in range(0,len(pushNotices)):
+        # See documentation on defining a message payload.
+        message = firebase_admin.messaging.Message(
+            notification=messaging.Notification(
+                title=pushNoticeTitles[i],
+                body='새로운 공지사항',
+            ),
+            token=key,
+        )
+        response = firebase_admin.messaging.send(message)
+        print('Successfully sent message:', response)
+
+
 for i in range(len(notice.IDNumbers)):
     root.child('Seoul_Bluemir/Notices').update({
         notice.IDNumbers[i] : {
@@ -21,3 +52,9 @@ for i in range(len(notice.IDNumbers)):
             'Date' : (notice.Dates)[i]
         }
     })
+
+
+
+
+
+
